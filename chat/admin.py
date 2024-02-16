b566b8bbf65d10cadfce53ff575da80c624ae102
MWorksCoding <mail.org@posteo.org>
2024-02-16 22:34:03 +0100

    first post request

--- chat/admin.py
+++ chat/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Message
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('chat','text','created_at','author')
-    list_display = ('text','created_at','author')
-    search_fields = ('text',)
+    fields = ('text','created_at','author', 'receiver')
+    list_display = ('text','created_at','author', 'receiver') # table view
+    search_fields = ('text',) # browse table
 # Decide which of the fields are displayed and in which order in the admin view, adding a search field
 
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
