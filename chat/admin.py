6a8420f7563d0c1e4900fc2b6607f7c928de3fa6
MWorksCoding <mail.org@posteo.org>
2024-02-16 22:12:03 +0100

    added model messages

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text','created_at','author')
+    list_display = ('text','created_at','author')
+    search_fields = ('text',)
+# Decide which of the fields are displayed and in which order in the admin view, adding a search field
+
+admin.site.register(Message)
