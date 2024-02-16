de9476c04f7d1d8d75299f084cfb863f24827926
MWorksCoding <mail.org@posteo.org>
2024-02-16 23:11:20 +0100

    added data base with chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Chat
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text','created_at','author', 'receiver')
-    list_display = ('text','created_at','author', 'receiver') # table view
+    fields = ('chat', 'text','created_at','author', 'receiver')
+    list_display = ('chat', 'text','created_at','author', 'receiver') # table view
     search_fields = ('text',) # browse table
 # Decide which of the fields are displayed and in which order in the admin view, adding a search field
 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
