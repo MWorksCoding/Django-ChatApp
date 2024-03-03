96c30a5632f7f4477e9171f1af2114325acb3f1d
MWorksCoding <mail.org@posteo.org>
2024-03-03 18:44:20 +0100

    added chat channels

--- chat/views.py
+++ chat/views.py
@@ -9,18 +9,54 @@ from django.core import serializers
 
 
 @login_required(login_url='/login/')#redirect if not logged in
-def index(request):    
+def index(request):  
+    
+    # if request.path.endswith('/chat/'):
+    #     return redirect('/chat/django')
+    # get url name
+    print('request.path:', request.path)
+    index_chat = request.path.find("/chat/")
+    if index_chat != -1: # Extract the substring after "/chat/"
+        chat_name = request.path[index_chat + len("/chat/"):]
+
+    else:
+        print("Not found in the path.")
+        chat_name = ''
+    
+    if chat_name == '':
+        chat_name == 'django'
+    
+    
+    print('chat_name:' , chat_name)  # Output: python
+    
+    chat_object = 'default object'
+    
+    if chat_name == 'django':
+        chat_object = 1
+    elif chat_name == 'news':
+        chat_object = 2
+    elif chat_name == 'introduction':
+        chat_object = 3
+    elif chat_name == 'help':
+        chat_object = 4
+    else:
+        chat_object = 1  # Default to 'django' if no chat room is specified
+    
+    print("Chat object:", chat_object)
+        
+    
     if request.method == 'POST':
         print("Received data " + request.POST['textmessage'])
-        mychat = Chat.objects.get(id=1)
+        mychat = Chat.objects.get(id=chat_object)
+        print('mychat' , mychat)
         new_message = Message.objects.create(text=request.POST['textmessage'] ,chat=mychat, author=request.user, receiver=request.user)
         serialized_obj = serializers.serialize('json', [new_message, ])
         return JsonResponse(serialized_obj[1:-1], safe=False)
-    chatMessages = Message.objects.filter(chat__id=1)
-    return render(request, 'chat/index.html', {'messages': chatMessages })
+    chatMessages = Message.objects.filter(chat__id=chat_object)
+    return render(request, 'chat/index.html', {'messages': chatMessages, 'chat_name' : chat_name })
 
 def login_view(request):
-    redirect_to = request.GET.get('next', '/chat/')  # Default redirect URL after login
+    redirect_to = request.GET.get('next', '/chat/django')  # Default redirect URL after login
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
